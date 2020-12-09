cda2fac557896ffb410348534949c4247d842514
unknown <erictan04298@gmail.com>
2020-12-09 16:51:32 +1300

    change routes/uploadingImg.js change file path

--- routes/uploadImg.js
+++ routes/uploadImg.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
+const path = require("path");
 
 const storage = multer.diskStorage({
 	destination: (req, file, cb) => {
-		cb(null, "./uploads");
+		cb(null, path.join(__dirname, ".", "uploads"));
 	},
 	filename: (req, file, cb) => {
 		cb(null, file.originalname);
